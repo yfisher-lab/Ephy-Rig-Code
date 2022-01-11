b0be5f300973646a27cc52dbdb31a7b742c7d1ce
Yvette <yfisher@berkeley.edu>
2022-01-11 12:03:02 -0800

    Created makeInjectionWaveform.m

---
+++ makeInjectionWaveform.m
@@ -0,0 +1,5 @@
+function [outputArg1] = makeInjectionWaveform(inputArg1,inputArg2)
+%MAKEINJECTIONWAVEFORM Summary of this function goes here
+%   Detailed explanation goes here
+outputArg1 = inputArg1;
+end
